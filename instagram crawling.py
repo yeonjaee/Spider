85eefe640719671c316a7cfc9c930283c543cb01
syj <siriyay@naver.com>
2022-02-07 10:56:47 +0900

    add py

--- instagram crawling.py
+++ instagram crawling.py
@@ -1,26 +1,39 @@
 from selenium import webdriver
+import instagram_def as idef    # 함수 파일 참조
 import pandas as pd
 import unicodedata
 import time
 import re
 
-driver = webdriver.Chrome('C:/Users/yj/Desktop/datasalon/chromedriver.exe')
+# 주의
+# python myscript.py 로 실행하면 화면 안꺼짐.
+
+# login
+#   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #
 options = webdriver.ChromeOptions()
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
-driver = webdriver.Chrome(options=options)
+driver = webdriver.Chrome(
+    'C:/Users/yj/Desktop/datasalon/chromedriver.exe', options=options)
 driver.get('https://www.instagram.com')
+
 time.sleep(3)
 
-print('Please enter your INSTAGRAM PASSWORD: ')
-id = input()
+id = input("Please enter your INSTAGRAM ID: ")
 input_id = driver.find_elements_by_css_selector('input._2hvTZ.pexuQ.zyHYP')[0]
 input_id.clear()
 input_id.send_keys(id)
 
-print('Please enter your INSTAGRAM PASSWORD: ')
-password = input()
+password = input("Please enter your INSTAGRAM PASSWORD: ")
 input_pw = driver.find_elements_by_css_selector('input._2hvTZ.pexuQ.zyHYP')[1]
 input_pw.clear()
 input_pw.send_keys(password)
 input_pw.submit()
+
 time.sleep(3)
+#   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #
+
+# search
+#   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #
+word = input("Please enter the keyword to search: ")
+url = idef.insta_searching(word)
+driver.get(url)
---
+++ instagram_def.py
@@ -0,0 +1,3 @@
+def insta_searching(word):
+    url = 'https://www.instagram.com/explore/tags/'+word
+    return url
